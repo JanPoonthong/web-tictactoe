7142f3f6f69b40712eec87e25370bab7bfabae84
Jan Poonthong <janpoonthong628@gmail.com>
2022-11-08 02:07:09 +0700

    Updates

---
+++ precache-manifest.09b080a7934895a9399bfce438c195de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e161bb2361d033091713",
+    "url": "/web-tictactoe/static/css/main.f49ff389.chunk.css"
+  },
+  {
+    "revision": "e161bb2361d033091713",
+    "url": "/web-tictactoe/static/js/main.e161bb23.chunk.js"
+  },
+  {
+    "revision": "ca13be8b904b75911516",
+    "url": "/web-tictactoe/static/js/1.ca13be8b.chunk.js"
+  },
+  {
+    "revision": "85748a60c3d89dcf8d90",
+    "url": "/web-tictactoe/static/js/runtime~main.85748a60.js"
+  },
+  {
+    "revision": "f44d1f490626e9e35164241836b99620",
+    "url": "/web-tictactoe/index.html"
+  }
+];
